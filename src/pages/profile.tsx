1080a451076f8ce28ab47a5e54e066946d5120b1
Bhuvneshwari-Rathore2001 <bhuvneshwarirathore4@gmail.com>
2025-01-04 14:07:47 +0530

    added user placeholder

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,40 +1,25 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
-import { IUser } from "../types/user";
+import { useSelector } from '../redux/store';
 
 const Profile = () => {
-  const [user, setUser] = useState<IUser>()
-
-  useEffect(() => {
-    try 
-    {
-    const fetchUser = async()=>{
-      const {data} = await axios.get('http://localhost:4000/api/v1/me',{
-        withCredentials:true
-      })
-      const {success,user} = data
-      if(success){
-        setUser(user)
-      }
-      }
-    fetchUser();
-    }
-    catch (error) {
-      console.log(error)
-    }
-  
-    
-  }, [])
-  
+  const { user } = useSelector((state) => state.user);
 
   return (
-
     <div className='flex items-center justify-center max-w-5xl m-auto h-[calc(100vh-84px)]'>
       <div className='flex gap-20 flex-1 items-center'>
         <div className='w-full flex flex-col gap-3'>
-          <img src={user?.avatar.url} alt='avatar' />
-          <input type='file' />
-
+          <div className='relative group'>
+            <img
+              src={user?.avatar.url || '/assets/user_placeholder.jpg'}
+              alt='avatar'
+            />
+            <label
+              htmlFor='avatar'
+              className='absolute top-0 h-full w-full flex justify-center items-center group-hover:visible invisible transition-all bg-black/25 hover:cursor-pointer rounded'
+            >
+              Choose Image
+            </label>
+            <input type='file' id='avatar' className='invisible' />
+          </div>
           <button className='bg-[rgba(0,0,0,0.7)] text-white hover:bg-black hover:text-pink-500 py-2 px-3'>
             Edit Profile
           </button>
